d5e8c56afe71edfb54b14669dc213f722f755ba5
patriagani <patria.gani@gmail.com>
2018-11-26 14:23:46 +0700

    roman numerals recursive done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,32 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  let numStr = String(input)
+  let number = input
+  let ribuanDic = ["","M","MM","MMM"]
+  let ratusanDic = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+  let puluhanDic = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+  let satuanDic = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+  let numberDic = ["0","1","2","3","4","5","6","7","8","9"]
+  let result = ""
+
+  if (numStr.length === 4) {
+    result = ribuanDic[numberDic.indexOf(numStr[0])]
+    let pengurang = Math.floor(number/1000)*1000
+    return result + toRoman(number - pengurang)
+  }
+  else if (numStr.length === 3) {
+    result = ratusanDic[numberDic.indexOf(numStr[0])]
+    let pengurang = Math.floor(number/100)*100
+    return result + toRoman(number - pengurang)
+  }
+  else if (numStr.length === 2) {
+    result = puluhanDic[numberDic.indexOf(numStr[0])]
+    let pengurang = Math.floor(number/10)*10
+    return result + toRoman(number - pengurang)
+  }
+  else if (numStr.length === 1) {
+    result = satuanDic[numberDic.indexOf(numStr[0])]
+    return result
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
