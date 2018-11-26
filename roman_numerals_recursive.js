e123b3a006cf2c4047b08b1fe809e8a9d7178c8f
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-26 15:20:35 +0700

    Add Recursive roman numeral

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,16 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var Roman=['M','DM','D','CD','C','LC','L','XL','X','IX','V','IV','I']
+  var Num=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  if(input==0){
+    return ''
+  }else{
+    for(var i=0;i<Roman.length;i++){
+      if(input >= Num[i]){
+        return Roman[i] + toRoman(input-Num[i])
+      }
+    }
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
