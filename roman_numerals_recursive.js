07fb4861161068f373d3a83568fa6519d9e72276
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-26 20:14:52 +0700

    Rapiin

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,11 +1,11 @@
 function toRoman(input) {
   // start your code here
-  var Roman=['M','DM','D','CD','C','LC','L','XL','X','IX','V','IV','I']
-  var Num=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
-  if(input==0){
+  var Roman=['M', 'DM', 'D', 'CD', 'C', 'LC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  var Num=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  if(input == 0) {
     return ''
   }else{
-    for(var i=0;i<Roman.length;i++){
+    for(var i=0; i < Roman.length; i++){
       if(input >= Num[i]){
         return Roman[i] + toRoman(input-Num[i])
       }
