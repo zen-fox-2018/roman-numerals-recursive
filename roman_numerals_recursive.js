9b86c9765407a46e5d83f8c1e8fccb83be5babc4
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-26 17:20:34 +0700

    komitt 2

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,40 +1,45 @@
-function toRoman(input) {
-  const ribuan = ["", "M", "MM", "MMM"]
-  const ratusan = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-  const puluhan = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-  const satuan = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-  const numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  let numberStr = String(input)
-  let index = numbersArr.indexOf(numberStr[0])
-
-  switch (numberStr.length) {
-    case 1:
-      return satuan[index]
-      break;
-
-    case 2:
-      return puluhan[index] + toRoman(numberStr.slice(1, numberStr.length))
-      break;
-
-    case 3:
-      return ratusan[index] + toRoman(numberStr.slice(1, numberStr.length))
-      break;
-
-    case 4:
-      return ribuan[index] + toRoman(numberStr.slice(1, numberStr.length))
-
-      break;
+function toRoman(num) {
+  let rom = ['M','CM','DC','D','CD','C','LX','L','XL','X','IX','VI','V','IV','I']
+  let number = [1000, 900, 600, 500, 400, 100, 90, 60, 50, 40, 10, 9, 6, 5, 4, 1]
+  let result = ''
+  
+  let obj = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    // LX: 60,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    // VI: 6,
+    V: 5,
+    IV: 4,
+    I: 1
   }
 
+  for (const key in obj) {
+    while (num >= obj[key]) {
+      return (result += key) + toRoman(num -= obj[key])
+    }
+  }
+
+  return result
+  
 }
 
+// Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
-console.log('——————|——————————|————————')
+console.log('——————|——————————|———————')
 console.log('4     | IV       | ', toRoman(4))
 console.log('9     | IX       | ', toRoman(9))
-console.log('23    | XXIII    | ', toRoman(23))
+console.log('13    | XIII     | ', toRoman(13))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
+
 
 module.exports = toRoman
