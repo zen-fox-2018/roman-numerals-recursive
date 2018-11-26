a70a75d50c5b32efacc40177beae0e2381f68ceb
fauzan_bintang <obin.aji@gmail.com>
2018-11-26 19:33:14 +0700

    done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,15 +1,30 @@
-const roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
-const number = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+// const roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+// const number = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+
+// function toRoman(input) {
+//   if (!input) {
+//     return ''
+//   } else {
+//     for (let i = number.length-1; i >= 0; i--) {
+//       if (input >= number[i]) {
+//         return roman[i] + toRoman(input - number[i])
+//       }
+//     }
+//   }
+// }
+
+// pake .findIndex
+const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I',]
+const number = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
 function toRoman(input) {
-  if (input == 0) {
+  if (!input) {
     return ''
   } else {
-    for (let i = number.length-1; i >= 0; i--) {
-      if (input >= number[i]) {
-        return roman[i] + toRoman(input - number[i])
-      }
-    }
+    let index = number.findIndex((val) => {
+      return val <= input
+    })
+    return roman[index] + toRoman(input - number[index])
   }
 }
 
