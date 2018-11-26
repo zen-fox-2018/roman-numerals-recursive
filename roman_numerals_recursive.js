61ed7ae6fb39ebf19061d60064092c851ecc8b2d
rachmadza <razato.zai@gmail.com>
2018-11-26 20:08:01 +0700

    push anchor roman numerals recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  // return to_roman(input);
+
+  var arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  var romawi = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+
+  if (input > 0) {
+    for (let i = 0; i < arab.length; i++) {
+      if (input >= arab[i]) {
+        return romawi[i] + toRoman(input - arab[i])
+      }
+    }
+  } else {
+    return ''
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
