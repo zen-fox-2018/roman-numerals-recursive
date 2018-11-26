a47b36d54e2674d8235050c2477464a5a9364640
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-26 17:12:47 +0700

    submit romanrec.js

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,15 +1,22 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  var listNum = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  if (input === 0) {
+    return '';
+  }
+  for (var i = 0; i < listNum.length; i++) {
+    if (input >= listNum[i]) {
+      return romanNum[i] + toRoman(input - listNum[i]);
+    }
+  }
 }
 
-console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|————————')
+// console.log('My totally sweet testing script for new roman\n')
+// console.log('input | expected | actual')
+// console.log('——————|——————————|————————')
 console.log('4     | IV       | ', toRoman(4))
 console.log('9     | IX       | ', toRoman(9))
 console.log('23    | XXIII    | ', toRoman(23))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
-
-module.exports = toRoman
