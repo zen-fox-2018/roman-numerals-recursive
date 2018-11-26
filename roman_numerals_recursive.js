2d3079a9c25d41ea33fc5d1c7c7014eb7f28ef3b
Aditya Respati <AdityaRespati>
2018-11-26 21:07:25 +0700

    added new file

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,28 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var ones=["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
+  var tens=["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"]
+  var hundreds=["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "M"]
+
+  var str=String(input)
+
+  if(str.length === 1){
+    return ones[Number(str[0])-1] 
+  } 
+  else if(input>=1000){
+    var millions= ""
+    for(var i=1; i<=Number(str[0]); i++){
+      millions += "M" 
+    }
+    return millions + toRoman(Number(str.slice(1)))
+  }
+  else if(input>=100){
+    return hundreds[Number(str[0])-1] + toRoman(Number(str.slice(1)))
+  }
+  else if(input>=10){
+    return tens[Number(str[0])-1] + toRoman(Number(str.slice(1)))
+  }
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
